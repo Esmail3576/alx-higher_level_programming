25b19dbdd93fc980036e6afb4d71ae6dfcd569ce
Esmail3576 <esmaeel3576@gmail.com>
2024-01-29 10:21:32 +0000

    another try by me put every line in a variable then print it

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,3 +1,42 @@
 #!/usr/bin/python3
-zen_of_python = "Beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex. Complex is better than complicated. Flat is better than nested. Sparse is better than dense. Readability counts. Special cases aren't special enough to break the rules. Although practicality beats purity. Errors should never pass silently. Unless explicitly silenced. In the face of ambiguity, refuse the temptation to guess. There should be one-- and preferably only one --obvious way to do it. Although that way may not be obvious at first unless you're Dutch. Now is better than never. Although never is often better than right now. If the implementation is hard to explain, it's a bad idea. If the implementation is easy to explain, it may be a good idea. Namespaces are one honking great idea -- let's do more of those!"
-print(zen_of_python)
+a = 'The Zen of Python, by Tim Peters' 
+b = 'Beautiful is better than ugly.'
+c = 'Explicit is better than implicit.'
+d = 'Simple is better than complex.'
+e = 'Complex is better than complicated.'
+f = 'Flat is better than nested.'
+g = 'Sparse is better than dense.'
+h = 'Readability counts.'
+i = 'Special cases aren\'t special enough to break the rules.'
+j = 'Although practicality beats purity.'
+k = 'Errors should never pass silently.'
+l = 'Unless explicitly silenced.'
+m = 'In the face of ambiguity, refuse the temptation to guess.'
+n = 'There should be one-- and preferably only one --obvious way to do it.'
+o = 'Although that way may not be obvious at first unless you\'re Dutch.'
+p = 'Now is better than never.'
+q = 'Although never is often better than right now.'
+r = 'If the implementation is hard to explain, it\'s a bad idea.'
+s = 'If the implementation is easy to explain, it may be a good idea.'
+t = 'Namespaces are one honking great idea -- let\'s do more of those!'
+print(f"""{a}
+
+{b}
+{c}
+{d}
+{e}
+{f}
+{g}
+{h}
+{i}
+{j}
+{k}
+{l}
+{m}
+{n}
+{o}
+{p}
+{q}
+{r}
+{s}
+{t}""")
