a9f5d28d59f30235c793b489435eb6b3e5ed463c
Esmail3576 <esmaeel3576@gmail.com>
2024-03-16 10:50:40 +0200

    update readme file and add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+"""
+This module defines
+the sqaure class and methods
+"""
+
+
+class Square:
+    """
+    Defines a square class with
+    private instance attribute size
+    and make validations
+    to accept only positive integers
+    for size and must be greater than 0
+    """
+
+    def __init__(self, size=0):
+        """Initialize with private instance attribute size"""
+        self.__size = size
+        try:
+            if self.__size is int:
+                pass
+        except TypeError:
+            raise TypeError("size must be an integer")
+        try:
+            if self.__size < 0:
+                pass
+        except ValueError:
+            raise ValueError("size must be >= 0")
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,4 +1,8 @@
 ### 0x06. Python - Classes and Objects
-    learning about oop concepts and learn some magic methods like __str__ and __repr__ and do some tasks with classes and objects
+    learning about oop concepts and learn some magic methods like __str__ and __repr__ and do some tasks with classes and objects that are listed below:
 ### 0. My first square
     an empty class Square that defines a square.
+### 1. Square with size
+    class Square that defines a square with private instance attribute size.
+### 2. Size validation
+    class Square that defines a square with private instance attribute size and make validation to check if there TypeError or ValueError.
