3a74bd86e262a233bb3da1bd18966a6969f5fa80
Esmail3576 <esmaeel3576@gmail.com>
2024-03-16 10:08:39 +0200

    fix docstring for module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Docstring for Square"""
 class Square:
-    """Docstring for Square"""
+    """define a square class"""
     def __init__(self, side):
         """Initialize a square object"""
         self.__side = side
