ef2ae74b17f1e8b208208f2910f2471faf06d4da
Esmail3576 <esmaeel3576@gmail.com>
2024-04-03 11:00:57 +0200

    6-print_comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for first_number in range(0, 8):
+    for second_number in range(first_number + 1, 10):
+        print("{}{}".format(first_number, second_number), end=', ')
+print("89")
