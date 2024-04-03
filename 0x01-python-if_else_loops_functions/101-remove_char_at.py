2d98ae29cae342f6645528c7924a388dd1ccc3b7
Esmail3576 <esmaeel3576@gmail.com>
2024-04-03 12:11:49 +0200

    fix the minus position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return str[:n] + str[n+1:]
+    if n < 0:
+        return str
+    elif n >= len(str):
+        return str
+    else:
+        return str[:n] + str[n+1:]
