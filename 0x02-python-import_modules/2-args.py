cec40233dea86a93a3b4bb5a587a14cefdda4fe3
Esmail3576 <esmaeel3576@gmail.com>
2024-04-06 15:37:06 +0200

    fix the first line in output 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,12 @@ if __name__ == "__main__":
 number = 0
 for arg in sys.argv:
     if number == 0:
-        pass
+        if len(sys.argv) > 2:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        else:
+            print("1 argument:")
+    elif number > 1:
+        print("{}: {}".format(number, arg))
     else:
         print("{}: {}".format(number, arg))
     number += 1
