5bc7fab197f726d2f2be4a9c65be5a2090b20d1f
Esmail3576 <esmaeel3576@gmail.com>
2024-04-06 15:57:22 +0200

    add 3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+index = 0
+result = 0
+for arg in sys.argv:
+    if len(sys.argv) == 1:
+        result = 0
+    elif index > 0:
+        number = int(arg)
+        result += number
+    index += 1
+
+print("{}".format(result))
