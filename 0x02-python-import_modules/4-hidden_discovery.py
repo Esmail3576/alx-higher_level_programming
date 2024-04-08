146336f7ac6804eb8f2bf05198d1dc5a78e84cb5
Esmail3576 <esmaeel3576@gmail.com>
2024-04-08 12:29:02 +0200

    add 4-hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import marshal
-with open("hidden_4.pyc", 'r') as hid:
-    hidden = marshal.load(hid)
-dir(hidden)
+    import hidden_4
+names = dir(hidden_4)
+for name in names:
+    if name[:2] != "__":
+        print(name)
